c508234bff73efcd73eb69570ab77cdec72de08f
jonathan <jonathan@101032330173.com>
2025-04-15 03:54:06 +0000

    First

---
+++ test_app.py
@@ -0,0 +1,8 @@
+import unittest
+from Documents.app import hello
+
+class TestApp(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(),"Hello, World!")
+if __name__ == "__main__":
+    unittest.main()
